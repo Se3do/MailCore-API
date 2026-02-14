5f86b6dc65836c0c47adc1ed886d168eb2762804
Mohammed <mohamedsaid6715@gmail.com>
2026-02-14 15:12:32 +0200

    Added Attachment Repository

---
+++ MailService.Domain/Interfaces/IAttachmentRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MailService.Domain.Interfaces
+{
+    using MailService.Domain.Entities;
+
+    public interface IAttachmentRepository
+    {
+        Task AddAsync(Attachment attachment, CancellationToken cancellationToken = default);
+        Task<Attachment?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
